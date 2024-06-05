63c4ab45f1dd6a19e14a252e6eaf32412e9a7ec0
Khyati Bardolia <khyati.bardolia@gmail.com>
2024-06-05 18:54:21 -0400

    feat: implement employee details page ui, add reducer function to get employee details

--- src/pages/EmployeeDetails/EmployeeDetails.tsx
+++ src/pages/EmployeeDetails/EmployeeDetails.tsx
@@ -1,6 +1,119 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { Box, Button, Avatar, Typography, styled } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'hooks';
+import { getEmployeeDetails } from 'store/slices/employeeSlice';
+import { RootState } from 'store';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import PersonIcon from '@mui/icons-material/Person';
+
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  width: theme.spacing(12),
+  height: theme.spacing(12),
+  backgroundColor: theme.palette.primary.main,
+  marginBottom: theme.spacing(2),
+}));
+
+const Title = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.secondary,
+  marginRight: theme.spacing(1),
+  fontSize: '1.5rem',
+}));
+
+const Value = styled(Typography)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  fontSize: '1.2rem',
+}));
 
 const EmployeeDetails: FC = () => {
-  return <>Employee Details</>;
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { id } = useParams<{ id: string }>();
+  const employee = useAppSelector(
+    (state: RootState) => state.employees.employee
+  );
+
+  const toggleEditMode = () => setIsEditMode((prev) => !prev);
+  const handleSave = () => {
+    toggleEditMode();
+  };
+
+  useEffect(() => {
+    if (id) {
+      dispatch(getEmployeeDetails(id));
+    }
+  }, [id, dispatch]);
+
+  if (!employee) {
+    return <Typography>Sorry, unable to fetch employee data.</Typography>;
+  }
+
+  return (
+    <Box sx={{ padding: 2 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          marginBottom: 12,
+        }}
+      >
+        <Button
+          variant="outlined"
+          onClick={() => navigate('/')}
+          startIcon={<ArrowBackIosIcon />}
+        >
+          Back
+        </Button>
+        <Typography variant="h4" align="center">
+          Employee Details
+        </Typography>
+        <Box sx={{ width: 1 / 6 }} /> {/* This is to balance the space */}
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          gap: '40px',
+        }}
+      >
+        <StyledAvatar>
+          <PersonIcon sx={{ fontSize: 60 }} />
+        </StyledAvatar>
+        <Box sx={{ fontSize: '2rem', width: '100%', maxWidth: 400 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
+            <Title>Name:</Title>
+            <Value>{employee.name}</Value>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
+            <Title>Age:</Title>
+            <Value>{employee.age}</Value>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
+            <Title>Relationship:</Title>
+            <Value>{employee.relationship}</Value>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
+            <Title>Type of Work:</Title>
+            <Value>{employee.typeOfWork}</Value>
+          </Box>
+        </Box>
+        <Box mt={2}>
+          {isEditMode ? (
+            <Button variant="contained" onClick={handleSave}>
+              Save
+            </Button>
+          ) : (
+            <Button variant="contained" onClick={toggleEditMode}>
+              Edit
+            </Button>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
 };
+
 export default EmployeeDetails;
--- src/store/slices/employeeSlice.ts
+++ src/store/slices/employeeSlice.ts
@@ -1,19 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  employees: [],
+  employees: JSON.parse(localStorage.getItem('employees')) || [],
+  employee: {},
 };
 
 const employeeSlice = createSlice({
   name: 'employees',
   initialState,
   reducers: {
-    addEmployee: (state, action) => {
+    addEmployee(state, action) {
       state.employees.push(action.payload);
+      localStorage.setItem('employees', JSON.stringify(state.employees));
+    },
+    getEmployeeDetails(state, action) {
+      if (state.employees?.length) {
+        state.employee = state.employees.find(
+          (emp) => emp.id === action.payload
+        );
+      }
     },
   },
 });
 
-export const { addEmployee } = employeeSlice.actions;
+export const { addEmployee, getEmployeeDetails } = employeeSlice.actions;
 
 export default employeeSlice.reducer;
